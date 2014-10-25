7f420edfa01be0f0618977d0d026f088ec0b5d0c
Mike Blaszczak <mikeblas@gmail.com>
2014-10-24 20:12:42 -0700

    More refactoring
    Break out User and LargestString into their own files

---
+++ LargestString.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikiReader
+{
+    class LargestString
+    {
+        String _current;
+        String _largest;
+        String _name;
+
+        public LargestString(String name)
+        {
+            _name = name;
+        }
+
+        public void test(String str)
+        {
+        }
+
+        public void Reset()
+        {
+            _current = null;
+            _largest = null;
+        }
+
+        public String Name
+        {
+            get { return _name; }
+        }
+
+        public String Current
+        {
+            get { return _current; }
+            set
+            {
+                if (_largest == null || value.Length > _largest.Length)
+                {
+                    _largest = value;
+                }
+                _current = value;
+            }
+        }
+
+        public String Largest
+        {
+            get { return _largest; }
+        }
+    }
+
+}
---
+++ User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikiReader
+{
+    /// <summary>
+    /// Represents a user; contains their name (as a string) and an ID integer.
+    /// </summary>
+    class User
+    {
+        String _userName = null;
+        Int64 _userId = 0;
+
+        /// <summary>
+        /// Create a User object
+        /// </summary>
+        /// <param name="userId">user ID integer</param>
+        /// <param name="userName">user name as a string</param>
+        public User(Int64 userId, String userName)
+        {
+            _userId = userId;
+            _userName = userName;
+        }
+    }
+}
