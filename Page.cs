694b064d77eb87408529c46d46e31d0456667363
Mike Blaszczak <mikeblas@gmail.com>
2014-10-24 20:10:06 -0700

    More refactoring
    Split Page and PageRevision into their own files

---
+++ Page.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikiReader
+{
+    /// <summary>
+    /// Class that represents a single page. It directly holds the page name, the page's namespaceID,
+    /// and the pageID. It contains a list of zero or more revisions of the page,
+    /// each an instance of the PageRevision class.
+    /// </summary>
+    class Page
+    {
+        String _pageName = null;
+        int _namespaceId = 0;
+        Int64 _pageId = 0;
+
+        SortedList<Int64, PageRevision> revisions = new SortedList<Int64, PageRevision>();
+
+        /// <summary>
+        /// Create a new Page instance
+        /// </summary>
+        /// <param name="namespaceId">namespaceId where this page lives</param>
+        /// <param name="pageId">pageID for this page</param>
+        /// <param name="pageName">name of this page</param>
+        public Page(int namespaceId, Int64 pageId, String pageName)
+        {
+            _namespaceId = namespaceId;
+            _pageName = pageName;
+            _pageId = pageId;
+        }
+
+        /// <summary>
+        /// add a revision to the history of this page
+        /// </summary>
+        /// <param name="pr"></param>
+        public void AddRevision(PageRevision pr)
+        {
+            if (revisions.ContainsKey(pr.revisionId))
+            {
+                System.Console.WriteLine("Page {0} already has revision {1}", _pageName, pr.revisionId);
+                System.Console.WriteLine("current: {0} with id {1}, parent id {2}",
+                    revisions[pr.revisionId].timestamp, revisions[pr.revisionId].revisionId, revisions[pr.revisionId].parentRevisionId);
+                System.Console.WriteLine("    new: {0} with id {1}, parent id {2}",
+                    pr.timestamp, pr.revisionId, pr.parentRevisionId);
+
+            }
+            revisions.Add(pr.revisionId, pr);
+            if (revisions.Count % 100 == 0)
+            {
+                CloseRevisions();
+            }
+        }
+
+        public void CloseRevisions()
+        {
+            for (int n = 0; n < revisions.Count - 1; n++)
+            {
+                revisions.Values[n].text = null;
+            }
+        }
+    }
+
+}
---
+++ PageRevision.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikiReader
+{
+    /// <summary>
+    /// Represents a revision to a Page.
+    /// 
+    /// https://www.mediawiki.org/wiki/Manual:Revision_table
+    /// 
+    /// </summary>
+    class PageRevision
+    {
+        Int64 _parentRevisionId = 0;
+        Int64 _revisionId = 0;
+        DateTime _timestamp = DateTime.MinValue;
+        User _contributor = null;
+        String _comment = null;
+        String _text = null;
+        int _textLength = 0;
+        bool _minor = false;
+        bool _commentDeleted = false;
+        bool _textDeleted = false;
+
+        public PageRevision(Int64 parentRevisionId, Int64 revisionId, DateTime timestamp, User contributor, String comment, String text, bool minor)
+        {
+            _parentRevisionId = parentRevisionId;
+            _revisionId = revisionId;
+            _timestamp = timestamp;
+            _contributor = contributor;
+            _comment = comment;
+            _text = text;
+            _textLength = _text.Length;
+            _minor = minor;
+        }
+
+        public Int64 revisionId
+        {
+            get { return _revisionId; }
+        }
+
+        public Int64 parentRevisionId
+        {
+            get { return _parentRevisionId; }
+        }
+
+        public User contributor
+        {
+            get { return _contributor; }
+        }
+
+        public DateTime timestamp
+        {
+            get { return _timestamp; }
+        }
+
+        public String comment
+        {
+            get { return _comment; }
+            set { _comment = comment; }
+        }
+
+        public String text
+        {
+            get { return _text; }
+            set
+            {
+                _text = value;
+                if (value != null) _textLength = _text.Length;
+            }
+        }
+    }
+
+}
