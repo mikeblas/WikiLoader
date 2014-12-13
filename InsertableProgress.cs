0d18b98eb994fd4ef95406a7208df587fa1e27f7
Mike Blaszczak <mikeblas@gmail.com>
2014-12-13 09:59:37 -0800

    Comments and counters

---
+++ InsertableProgress.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikiReader
+{
+    interface InsertableProgress
+    {
+        void AddPendingRevisions(int count);
+        void CompleteRevisions(int count);
+
+        void InsertedPages(int count);
+        void InsertedUsers(int count);
+        void InsertedRevisions(int Count);
+    }
+}
