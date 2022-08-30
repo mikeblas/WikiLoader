f5cb6a287a672b5fe26395c5579a594e0b4ca726
Mike Blaszczak <mikeblas@gmail.com>
2022-08-30 07:57:34 -0700

    update revision count progress

--- Page.cs
+++ Page.cs
@@ -426,6 +426,8 @@ namespace WikiReader
                     Console.WriteLine("Exception!!!");
                 }
             }
+
+            progress.CompleteRevisions(revisions.Count);
         }
 
 
